9fc8b9287c6b5ebd7e06e973e8c39f6c0bd5e489
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-17 15:02:06 +0530

    Added About and Resources pages to the navigation bar

--- ttonline/src/App.jsx
+++ ttonline/src/App.jsx
@@ -3,17 +3,21 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Landing from "./pages/Landing"
 import PetitionPage from "./pages/PetitionPage"
+import About from "./pages/About"
+import Resources from "./pages/Resources"
 const App = () => {
   return (
     <>
-     <BrowserRouter>
-     <Header/>
-      <Routes>
-        <Route path="/" element={<Landing/>} exact/>
-        <Route path="/petition" element={<PetitionPage />} exact/>
-      </Routes>
-      <Footer/>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Landing />} exact />
+          <Route path="/petition" element={<PetitionPage />} exact />
+          <Route path="/about" element={<About />} exact />
+          <Route path="/resources" element={<Resources />} exact />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
