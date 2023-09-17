009cae3b76a83715ff0ee9974dee226fb973600e
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-17 14:50:47 +0530

    updated

--- ttonline/src/App.jsx
+++ ttonline/src/App.jsx
@@ -2,11 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Landing from "./pages/Landing"
-import Homepage from "./pages/Homepage"
 import PetitionPage from "./pages/PetitionPage"
-
-import Navbar from "./components/Navbar"
-
 const App = () => {
   return (
     <>
@@ -14,7 +10,7 @@ const App = () => {
      <Header/>
       <Routes>
         <Route path="/" element={<Landing/>} exact/>
-        <Route path="/petitions" element={<PetitionPage />} />
+        <Route path="/petition" element={<PetitionPage />} exact/>
       </Routes>
       <Footer/>
      </BrowserRouter>
